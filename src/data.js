d26dfedbaa935da1e406010a87e9b583b6fae441
fincorporated <finleyrosemcardle@gmail.com>
2025-10-27 09:48:04 -0400

    nav bar links

--- src/data.js
+++ src/data.js
@@ -1,22 +1,22 @@
 export const pages = [{
     id: 0,
     title: 'Home',
-    link: "https://fincorporated.github.io/portfolio/"
+    link: "https://fincorporated.com"
 }, {
     id: 1,
     title: 'About Me',
-    link: "https://fincorporated.github.io/portfolio/about",
+    link: "https://fincorporated.com/about",
 }, {
     id: 2,
     title: 'Projects',
-    link: "https://fincorporated.github.io/portfolio/projects",
+    link: "https://fincorporated.com/projects",
 }, {
     id: 3,
     title: 'Self-Teaching Blog',
-    link: "https://fincorporated.github.io/portfolio/blog",
+    link: "https://fincorporated.com/blog",
 }, {
     id: 4,
     title: 'Contact Me',
-    link: "https://fincorporated.github.io/portfolio/contact",
+    link: "https://fincorporated.com/contact",
 }
 ]
